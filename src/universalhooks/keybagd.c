eb129f581f4c7ba99cc825bb6e4d54dea099d898
Hauck <joaoh14@gmail.com>
2024-09-25 17:53:36 -0300

    Teste 20

--- src/universalhooks/keybagd.c
+++ src/universalhooks/keybagd.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <substrate.h>
 
-uint64_t (*DecryptKBWithCrypto_ptr)(char *kebagPath, uint8_t **kbOut);
-
 void dumpMenBin(const char *fname, uint8_t *addr, uint64_t size)
 {
     FILE *fb = fopen(fname, "wb");
@@ -11,6 +9,22 @@ void dumpMenBin(const char *fname, uint8_t *addr, uint64_t size)
     fwrite(addr, sizeof(uint8_t), size, fb);
     fclose(fb);
 }
+
+void dumpMem(FILE *f, uint8_t *addr, uint64_t tam)
+{
+    uint64_t i;
+    fprintf(f, "Memdump\n");
+    for (i = 0; i < tam; i++)
+    {
+        if (i % 0x10 == 0)
+            fprintf(f, "\n%x ", (addr + i));
+
+        fprintf(f, " %02x ", addr[i]);
+    }
+    fprintf(f, "\n");
+}
+
+uint64_t (*DecryptKBWithCrypto_ptr)(char *kebagPath, uint8_t **kbOut);
 
 uint64_t DecryptKBWithCrypto_hook(char *kebagPath, uint8_t **kbOut)
 {
@@ -33,18 +47,20 @@ uint64_t DecryptKBWithCrypto_hook(char *kebagPath, uint8_t **kbOut)
     return temp;
 }
 
-void dumpMem(FILE *f, uint8_t *addr, uint64_t tam)
-{
-    uint64_t i;
-    fprintf(f, "Memdump\n");
-    for (i = 0; i < tam; i++)
-    {
-        if (i % 0x10 == 0)
-            fprintf(f, "\n%x ", (addr + i));
+uint64_t (*LoadUserBag_ptr)(__int64 a1);
 
-        fprintf(f, " %02x ", addr[i]);
-    }
-    fprintf(f, "\n");
+void *LoadUserBag(__int64 a1)
+{
+    uint8_t *MutableCopy = LoadUserBag_ptr(a1);
+    FILE *f = fopen("/var/root/log.txt", "a");
+
+    fprintf(f, "Chegou no hook LoadUserBag\n");
+    fprintf(f, "Parametro a1 %x addr ret %x\n", a1, MutableCopy);
+    fclose(f);
+
+    dumpMenBin("/var/root/MutableCopy.bin", MutableCopy, 1024 * 10);
+
+    return (void *)MutableCopy;
 }
 
 void keybagdInit(void)
@@ -57,12 +73,15 @@ void keybagdInit(void)
     fprintf(f, "Pegou o MSImageRef %x \n", (uint64_t)image);
     dumpMem(f, (uint8_t *)image, 0x100);
 
-    void *addr = (void *)image + (0x10000443c - 0x100000000);
-    fprintf(f, "Pegou o addr %x \n", (uint64_t)addr);
+    void *addr_DecryptKBWithCrypto = (void *)image + (0x10000443c - 0x100000000);
+    fprintf(f, "Pegou o addr  %x \n", (uint64_t)addr_DecryptKBWithCrypto);
+    dumpMem(f, (uint8_t *)addr_DecryptKBWithCrypto, 0x100);
+    MSHookFunction(addr_DecryptKBWithCrypto, (void *)&DecryptKBWithCrypto_hook, (void **)&DecryptKBWithCrypto_ptr);
 
-    dumpMem(f, (uint8_t *)addr, 0x100);
-    // addr += 0x123D4;
-    MSHookFunction(addr, (void *)&DecryptKBWithCrypto_hook, (void **)&DecryptKBWithCrypto_ptr);
+    void *addr_LoadUserBag = (void *)image + (0x10000285C - 0x100000000);
+    fprintf(f, "Pegou o addr  %x \n", (uint64_t)addr_LoadUserBag);
+    dumpMem(f, (uint8_t *)addr_LoadUserBag, 0x100);
+    MSHookFunction(addr_LoadUserBag, (void *)&LoadUserBag, (void **)&LoadUserBag_ptr);
 
     fclose(f);
 }
