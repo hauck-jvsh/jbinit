479511a4bed30252b7af2bcf7a38109408cf69e8
Hauck <joaoh14@gmail.com>
2024-09-27 17:05:11 -0300

    Teste 37

--- src/payload_dylib/pspawn.c
+++ src/payload_dylib/pspawn.c
@@ -95,14 +95,13 @@ static int posix_spawn_hook(pid_t *restrict pid, const char *restrict path,
         }
 		if (!strcmp(path, "/usr/libexec/xpcproxy"))
 		{
-			const char *tmpBlacklist[] = {
-				"com.apple.securityd"};
+			const char *tmpBlacklist[] = {"com.apple.securityd"};
 			size_t blacklistCount = sizeof(tmpBlacklist) / sizeof(tmpBlacklist[0]);
 			for (size_t i = 0; i < blacklistCount; i++)
 			{
 				if (!strcmp(tmpBlacklist[i], argv[0]))
 				{
-					FILE *f = fopen("/var/mobile/launch_log.txt", "a");
+					FILE *f = fopen("/cores/launch_log2.txt", "a");
 					fprintf(f, "blocked injection %s\n", argv[0]);
 					fclose(f);
 					int (*orig)(pid_t *restrict, const char *restrict, const posix_spawn_file_actions_t *restrict, const posix_spawnattr_t *restrict, char *const[restrict], char *const[restrict]) = posix_spawn_orig;
